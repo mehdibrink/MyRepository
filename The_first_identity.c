187ecac5535d838ed8fbe8e92611c28d910ee721
mehdibrink <72486963+mehdibrink@users.noreply.github.com>
2021-07-11 17:55:41 +0430

    Create The_first_identity.c

---
+++ The_first_identity.c
@@ -0,0 +1,35 @@
+char* first_identity(char* e){
+
+    char result[50]={};
+    char* presult=&result[0];
+    strcpy(result,e);
+
+    _Bool symbol=(int)strchr(result,'-');
+    int num1,num2=0;
+
+    num1=atoi(strtok(result,"+-x"));
+    num2=atoi(strtok(NULL,"+-x"));
+
+    if(num2==0){
+
+        num2=num1;
+        num1=1;
+        if(symbol)
+            sprintf(result,"x^2-%dx+%d",2*num1*num2,num2*num2);
+        else
+            sprintf(result,"x^2+%dx+%d",2*num1*num2,num2*num2);
+    }
+
+    else{
+        if(symbol)
+            sprintf(result,"%dx^2-%dx+%d",num1*num1,2*num1*num2,num2*num2);
+        else
+            sprintf(result,"%dx^2+%dx+%d",num1*num1,2*num1*num2,num2*num2);
+    }
+
+
+
+    return presult;
+
+}
+
